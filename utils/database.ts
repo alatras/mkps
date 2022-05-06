a07cd47d16313dc43596824c3c10fbc1fa74ca83
Alex A <atrashalex@gmail.com>
2022-05-06 09:53:40 +0100

    Add config & install packages

--- utils/database.ts
+++ utils/database.ts
@@ -1,8 +1,14 @@
+import { MongooseModuleOptions } from '@nestjs/mongoose'
+
 export const getMongoUri = () => {
-  let mongoUri = process.env.MONGODB_URI
+  const mongoUri = process.env.MONGODB_URI
 
   if (!mongoUri) {
     throw new Error('MONGODB_URI not set in env')
+  }
+
+  if (mongoUri === 'mongodb://localhost:27017') {
+    return `${mongoUri}?sslValidate=false&retryWrites=false&directConnection=true`
   }
 
   const urlParts = mongoUri.split('?')
@@ -14,7 +20,9 @@ export const getMongoUri = () => {
     query = `${query}&tlsCAFile=rds-combined-ca-bundle.pem`
   }
 
-  mongoUri = `${urlParts[0]}?retryWrites=false&directConnection=true&${query}`
+  return `${urlParts[0]}?retryWrites=false&directConnection=true&${query}`
+}
 
-  return mongoUri
+export const getMongoOptions = (): MongooseModuleOptions => {
+  return { dbName: process.env.MONGODB_NAME ?? undefined, uri: getMongoUri() }
 }
