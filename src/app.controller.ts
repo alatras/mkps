321db1fc9cdc2d5a5b1c941401931e7f4f01fd8a
Alex A <atrashalex@gmail.com>
2022-05-12 15:54:31 +0100

    Return version in '/' endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,11 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello()
+  getHello() {
+    return {
+      app: 'Marketplace API',
+      message: 'App Running',
+      version: this.appService.getVersion()
+    }
   }
 }
