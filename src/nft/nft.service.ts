57d8114ab589cbdf40329b83c2d22923d6e6af2c
Alex A <atrashalex@gmail.com>
2022-05-18 10:23:11 +0100

    Add NftService & NftController

--- src/nft/nft.service.ts
+++ src/nft/nft.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@nestjs/common'
 import { CreateNftDto } from './dto/nft.dto'
 import { InjectModel } from '@nestjs/mongoose'
-import { Nft, NftDocument } from './schemas/nft.schema'
+import { Nft, NftStatus } from './schemas/nft.schema'
 import { Model } from 'mongoose'
+import { from } from 'uuid-mongodb'
 
 @Injectable()
 export class NftService {
-  constructor(@InjectModel(Nft.name) private nftModel: Model<NftDocument>) {}
+  constructor(@InjectModel(Nft.name) private nftModel: Model<Nft>) {}
 
-  async create(createNftDto: CreateNftDto) {
-    return 'This action adds a new nft'
+  async create(userId: string, createNftDto: CreateNftDto) {
+    const nft = {
+      ...createNftDto,
+      isHidden: true,
+      owner: from(userId),
+      status: NftStatus.draft,
+      minterId: from(userId)
+    }
+
+    return (await this.nftModel.create(nft)).toObject()
   }
 }
